2bfe79fab77325ff738e2f513639ed1b433564a9
bskim617 <bskim617@gmail.com>
2023-04-10 11:16:45 +0900

    2023-04-10

---
+++ Ch05/src/components/sub4/Component1.vue
@@ -0,0 +1,13 @@
+<template>
+    <h4>Component1</h4>
+    <p>
+        query : {{ $route.query }}<br />
+        name : {{ $route.query.name }}<br />
+        age : {{ $route.query.age }}<br />
+        addr : {{ $route.query.addr }}<br />
+    </p>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ Ch05/src/components/sub4/Component2.vue
@@ -0,0 +1,13 @@
+<template>
+    <h4>Component2</h4>
+    <p>
+        params : {{ $route.params }}<br />
+        name : {{ $route.params.name }}<br />
+        age : {{ $route.params.age }}<br />
+        addr : {{ $route.params.addr }}<br />
+    </p>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
